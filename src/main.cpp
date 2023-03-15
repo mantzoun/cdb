966f7276510bb631195f803e3bad9bdcd7f8b9de
Stavros Mantzouneas <930338+mantzoun@users.noreply.github.com>
2023-03-15 17:53:22 +0200

    move main() code to a new class that extends message_cb, in order to prepare for a major refactoring ot the messaging system. Modules will send all messages to main(), and all processing and forwarding will take place there

--- src/main.cpp
+++ src/main.cpp
@@ -16,47 +16,63 @@
 #include "cdb_mqtt_handler.h"
 
 
+class Main : public cdb::CallbackClass
+{
+    private:
+        std::string config_file;
+        cdb::Logger * logger;
+        cdb::IO io;
+        cdb::Configurator conf;
+        cdb::MqttHandler m_handler;
+    public:
+        Main(int argc, char** argv) {
+            if (argc < 2){
+                std::cout << "Usage: cdb <config_file>\n";
+                exit(1);
+            }
+            config_file = argv[1];
+        }
+
+        void message_cb(cdb::callback_msg * msg)
+        {
+        }
+
+        int run(void) {
+            logger = new cdb::Logger(cdb::CDB_LOG_DEBUG);
+
+            conf.set_logger(logger);
+            conf.config_read(config_file);
+            logger->set_level(conf.get_log_level());
+
+            m_handler.set_logger(logger);
+            io.set_logger(logger);
+
+            LOG_INFO(logger, "Reading config file");
+
+            io.fifo_init("/tmp/mqtt_disc_cpp.fifo");
+
+            LOG_INFO(logger, "Starting Discord Bot");
+            cdb::DiscordBot bot(conf.discord_token());
+            bot.set_logger(logger);
+            bot.init(conf.discord_token(), conf.discord_bot_id());
+            bot.set_mqtt_handler(&m_handler);
+
+            m_handler.set_discord_bot(&bot);
+            m_handler.init(&conf);
+
+            io.set_discord_bot(&bot);
+
+            LOG_INFO(logger, "Init done, entering loop");
+
+            while(1){
+                usleep(1000000);
+            }
+        }
+};
 
 int main(int argc, char** argv)
 {
-    if (argc < 2){
-        std::cout << "Usage: cdb <config_file>\n";
-        exit(1);
-    }
+    Main obj(argc, argv);
 
-    std::string config_file = argv[1];
-
-    cdb::Logger logger = cdb::Logger(cdb::CDB_LOG_DEBUG);
-
-    cdb::IO io;
-    cdb::Configurator conf;
-    cdb::MqttHandler m_handler;
-
-    conf.set_logger(&logger);
-    conf.config_read(config_file);
-    logger.set_level(conf.get_log_level());
-
-    m_handler.set_logger(&logger);
-    io.set_logger(&logger);
-
-    LOG_INFO(&logger, "Reading config file");
-
-    io.fifo_init("/tmp/mqtt_disc_cpp.fifo");
-
-    LOG_INFO(&logger, "Starting Discord Bot");
-    cdb::DiscordBot bot(conf.discord_token());
-    bot.set_logger(&logger);
-    bot.init(conf.discord_token(), conf.discord_bot_id());
-    bot.set_mqtt_handler(&m_handler);
-
-    m_handler.set_discord_bot(&bot);
-    m_handler.init(&conf);
-
-    io.set_discord_bot(&bot);
-
-    LOG_INFO(&logger, "Init done, entering loop");
-
-    while(1){
-        usleep(1000000);
-    }
+    return obj.run();
 }
